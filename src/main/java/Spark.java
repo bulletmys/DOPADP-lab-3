abf6e3c647c23b885abfcc9adbf503d79176b71e
bulletmys <bulletmys@yandex.ru>
2019-11-14 22:39:42 +0300

    Scripted auto-commit on change (2019-11-14 22:39:42) by gitwatch.sh

--- src/main/java/Spark.java
+++ src/main/java/Spark.java
@@ -7,7 +7,7 @@ public class Spark {
         SparkConf conf = new SparkConf();
         JavaSparkContext sc = new JavaSparkContext(conf);
         System.out.println("START");
-        JavaRDD<String> flights = sc.textFile("Flights.csv");
+        JavaRDD<String> flights = sc.textFile("L_AIRPORT_ID.csv");
         JavaRDD<String> flights = sc.textFile("Flights.csv");
 
         System.out.println(flights_lines);
