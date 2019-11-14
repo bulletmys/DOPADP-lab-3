142007eaeabdec91fe8935562d6a9ec113d52fef
bulletmys <bulletmys@yandex.ru>
2019-11-14 23:22:48 +0300

    Scripted auto-commit on change (2019-11-14 23:22:48) by gitwatch.sh

--- src/main/java/Spark.java
+++ src/main/java/Spark.java
@@ -17,7 +17,7 @@ public class Spark {
 
         JavaRDD<String[]> parsedFlightsInfo = flightsParser.getStrings();
 
-        JavaPairRDD<Tuple2<String, String>, FlightsInfo> data = parsedFlightsInfo.red
+        JavaPairRDD<Tuple2<String, String>, FlightsInfo> data = parsedFlightsInfo.mapToPair()
 
 
     }
