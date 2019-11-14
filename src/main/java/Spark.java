56fff7b3b978bb8e39e35f9201da4f3c8c6fa67f
bulletmys <bulletmys@yandex.ru>
2019-11-14 22:38:47 +0300

    Scripted auto-commit on change (2019-11-14 22:38:47) by gitwatch.sh

--- src/main/java/Spark.java
+++ src/main/java/Spark.java
@@ -6,8 +6,10 @@ public class Spark {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf();
         JavaSparkContext sc = new JavaSparkContext(conf);
+        System.out.println("KEKLOL ARBIDOL");
         JavaRDD<String> flights = sc.textFile("Flights.csv");
 
+        
         JavaRDD<String[]> flights_lines = flights.map(s -> s.replaceAll("\"", "").split(",")).filter(strings -> !strings[0].equals("Flights"));
         System.out.println(flights_lines);
     }
