dd5374299f5d5ed50f66a4357e017e81bd8e16a6
bulletmys <bulletmys@yandex.ru>
2019-11-14 22:52:16 +0300

    Scripted auto-commit on change (2019-11-14 22:52:16) by gitwatch.sh

--- src/main/java/FlightsInfo.java
+++ src/main/java/FlightsInfo.java
@@ -1,11 +1,32 @@
 import java.io.Serializable;
 
-public class Parser implements Serializable {
+public class FlightsInfo implements Serializable {
     private float maxDelayedTime;
     private int numOfDelayed;
     private int numOfCanceled;
     private int numOfAll;
 
-    Parser()
+    FlightsInfo(float maxDelayedTime, int numOfDelayed, int numOfCanceled, int numOfAll) {
+        this.maxDelayedTime = maxDelayedTime;
+        this.numOfDelayed = numOfDelayed;
+        this.numOfCanceled = numOfCanceled;
+        this.numOfAll = numOfAll;
+    }
+
+    public float getMaxDelayedTime() {
+        return maxDelayedTime;
+    }
+
+    public int getNumOfAll() {
+        return numOfAll;
+    }
+
+    public int getNumOfCanceled() {
+        return numOfCanceled;
+    }
+
+    public int getNumOfDelayed() {
+        return numOfDelayed;
+    }
 
 }
