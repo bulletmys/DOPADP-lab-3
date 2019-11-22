99d7dbd812692446788727ff043af0fe339029f1
bulletmys <bulletmys@yandex.ru>
2019-11-23 02:40:02 +0300

    Scripted auto-commit on change (2019-11-23 02:40:02) by gitwatch.sh

--- src/main/java/SparkAdditional.java
+++ src/main/java/SparkAdditional.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class SparkAdditional { //Требуется составить для каждого аэропорта кол-во отмененных рейсов по дням неделей
     private static final int ORIGIN_AIRPORT_ID = 11;
     private static final int DEST_AIRPORT_ID = 14;
-    private static final int DAY_OF_WEEK = 5;
+    private static final int DAY_OF_WEEK = 4;
     private static final int CANCELLED = 19;
 
     private static Integer getOriginAirportID(String[] string) {
