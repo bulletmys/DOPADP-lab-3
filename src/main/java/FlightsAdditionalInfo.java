93ae79dc0b35699fbb83cbe55ac080802edffd6e
bulletmys <bulletmys@yandex.ru>
2019-11-23 02:19:24 +0300

    Scripted auto-commit on change (2019-11-23 02:19:24) by gitwatch.sh

--- src/main/java/FlightsAdditionalInfo.java
+++ src/main/java/FlightsAdditionalInfo.java
@@ -3,22 +3,12 @@ import java.util.ArrayList;
 
 public class FlightsAdditionalInfo implements Serializable {
 
-    private int[] canceled = new int[7];
+    private int[] canceled = new int[8];
 
     FlightsAdditionalInfo(Integer dayOfWeek, Integer numOfCanceled) {
         this.canceled[dayOfWeek] = numOfCanceled;
     }
-
-    public static FlightsAdditionalInfo sum(FlightsInfo o1, FlightsInfo o2) {
-        float maxDelayedTime = Math.max(o1.getMaxDelayedTime(), o2.getMaxDelayedTime());
-        int numOfDelayed = o1.getNumOfDelayed() + o2.getNumOfDelayed();
-        int numOfCanceled = o1.getNumOfCanceled() + o2.getNumOfCanceled();
-        int numOfAll = o1.getNumOfAll() + o2.getNumOfAll();
-        return new FlightsAdditionalInfo(maxDelayedTime, numOfDelayed, numOfCanceled, numOfAll);
-    }
-
-
-
+    
     public int getNumOfCanceled(int dayOfWeek) {
         if (dayOfWeek > 0)
         return canceled[dayOfWeek];
