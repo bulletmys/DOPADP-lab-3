1008ee42534d9f3db98a7650b64e1f6d1cedbe7a
bulletmys <bulletmys@yandex.ru>
2019-11-23 03:30:16 +0300

    Scripted auto-commit on change (2019-11-23 03:30:16) by gitwatch.sh

--- src/main/java/SparkAdditional.java
+++ src/main/java/SparkAdditional.java
@@ -33,7 +33,7 @@ public class SparkAdditional { //Требуется составить для к
         JavaRDD<String> airportsNames = sc.textFile("L_AIRPORT_ID.csv");
 
         FlightsParser flightsParser = new FlightsParser(flightsInfo);
-        JavaRDD<String[]> parsedFlightsInfo = flightsParser.getStrings().filter(strings -> strings[0].equals("YEAR"));
+        JavaRDD<String[]> parsedFlightsInfo = flightsParser.getStrings().filter(strings -> strings[20].equals("YEAR"));
 
         System.out.println(flightsInfo.collect().get(1));
         System.out.println(flightsInfo.collect().get(9));
